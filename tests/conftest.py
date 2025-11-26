48cf745e03c9d7ea0caec1991e632772fac0fef2
Denis Orzhenovskiy <thedamnation1044@yandex.ru>
2025-11-26 23:04:38 +0300

    Posts: Добавление тестов и правка общих фикстур.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def mock_config(mocker) -> None:
-    mocker.patch.dict('src.core.config.Config.model_config', {'env_file': '.test_env'})
+def default_config(mocker) -> None:
+    mocker.patch.dict('src.core.config.Config.model_config', {'env_file': None})
     mocker.patch.dict(
         'os.environ',
         {
@@ -15,12 +15,8 @@ def mock_config(mocker) -> None:
 
 
 @pytest.fixture
-def default_config(mocker) -> None:
-    mocker.patch.dict(
-        'os.environ',
-        {
-            'APP_NAME': 'Test App',
-            'DEBUG': 'True',
-            'DB_PATH': 'blog.sqlite3',
-        },
-    )
+def patch_config(mocker):
+    def _patch(env_vars: dict[str, str]) -> None:
+        mocker.patch.dict('os.environ', env_vars)
+
+    return _patch
--- tests/test_core/test_config.py
+++ tests/test_core/test_config.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pydantic
 import pytest
 
@@ -5,13 +7,39 @@ from src.core.initializer import init_config
 
 
 class TestInitConfig:
-    @pytest.mark.usefixtures('default_config')
+    @pytest.fixture
+    def temp_env(self, mocker):
+        env_file = pathlib.Path('.env.test')
+        env_content = """APP_NAME=Test App From Env
+DEBUG=False
+DB_PATH=blog_from_env.sqlite3"""
+
+        env_file.write_text(
+            env_content,
+            encoding='utf-8',
+        )
+
+        mocker.patch.dict('src.core.config.Config.model_config', {'env_file': env_file})
+        mocker.patch.dict('os.environ', {}, clear=True)
+
+        yield env_file
+
+        env_file.unlink()
+
     def test_init_successful(self) -> None:
         config = init_config()
 
         assert config.app_name == 'Test App'
         assert config.debug is True
         assert config.db_path == 'blog.sqlite3'
+
+    @pytest.mark.usefixtures('temp_env')
+    def test_init_from_env_file(self) -> None:
+        config = init_config()
+
+        assert config.app_name == 'Test App From Env'
+        assert config.debug is False
+        assert config.db_path == 'blog_from_env.sqlite3'
 
     def test_init_error(self, mocker) -> None:
         mocker.patch.dict('os.environ', {}, clear=True)
